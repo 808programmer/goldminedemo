711eed29e81bf764ade2f484937672591d7b9dcb
808programmer <francis.khalil07@gmail.com>
2025-04-04 22:42:56 -0400

    Removed django_browser_reload library

--- goldMineAI/settings.py
+++ goldMineAI/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'mining.apps.MiningConfig',
     'tailwind',
     'theme',
-    'django_browser_reload',
     'corsheaders',
 ]
 
@@ -57,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'goldMineAI.urls'
