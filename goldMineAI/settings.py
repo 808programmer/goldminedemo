054e364f429a56f7a6cf37ecde2bcd7a19cc56af
808programmer <francis.khalil07@gmail.com>
2025-04-04 22:23:08 -0400

    Updated requirements.txt

--- goldMineAI/settings.py
+++ goldMineAI/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
